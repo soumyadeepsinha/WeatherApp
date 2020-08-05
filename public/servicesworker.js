3640c5ec5eb15aa0c4def386f30ed2f869ac0d32
soumyadepsinha <s.sinha2701@gmail.com>
2020-08-05 11:42:27 +0530

    update app

--- public/servicesworker.js
+++ public/servicesworker.js
@@ -34,9 +34,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => Promise.all(
       cacheNames.map((cacheName) => {
-        if (!cacheWhitelist.includes(cacheName)) {
-          return caches.delete(cacheName);
-        }
+        return caches.delete(cacheName);
       })
     ))
 
